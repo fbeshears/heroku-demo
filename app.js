ec16e1fe3203726404818ba5638476aa4f46428b
Fred M Beshears <fmb@berkeley.edu>
2014-04-18 07:09:20 -0700

    modified greeting

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ port = process.env.PORT || 3000;
 
 server = http.createServer( function(request, response){
   response.writeHead( 200, { 'Content-Type': 'text/plain'});
-  response.end( 'Hello World' );
+  response.end( 'Hello There World' );
 }).listen( port );
 
 
